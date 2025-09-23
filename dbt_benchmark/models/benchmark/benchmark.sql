d0beacf9574264854d0f402fd5a8931f544bb985
santosh_nayak <34561766+santoshnayak-mi@users.noreply.github.com>
2025-09-23 13:33:57 -0400

    Updated code based on review

--- dbt_benchmark/models/benchmark/benchmark.sql
+++ dbt_benchmark/models/benchmark/benchmark.sql
@@ -4,15 +4,11 @@
 
 WITH raw_data AS (
 SELECT
-    cm.brand as platform,
     cm.geo as region,
     cm.Country,
     cm.industry as vertical,
-    v.account_id as account_id,
-    cm.account_name as account_name,
-    toDate(toStartOfMonth(send_date)),
+    toDate(toStartOfMonth(send_date)) as time_period,
     v.campaign_type,
-    '' as message_type,
     'Regular' as sto_type,
     v.channel as channel_type,
     sumIf(1, (event = 'message_send')) AS sends, 
@@ -20,38 +16,31 @@ SELECT
     sumIf(1, (event = 'message_soft_bounce')) AS unique_soft_bounces,
     sumIf(1, (event = 'message_hard_bounce')) AS unique_hard_bounces,
     uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email')) AS unique_opens,
-    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email')) AS unique_real_opens,
-    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email')) AS unique_precached_opens,
+    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email') AND (NOT is_machine)) AS unique_real_opens,
+    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email') AND (is_machine)) AS unique_precached_opens,
     sumIf(1, (event = 'message_click')) AS total_clicks,
     uniqMergeIf(member_state, (event = 'message_click') AND (channel = 'email')) AS unique_clicks,
-    0 as unique_linkclicks,
-    0 as unique_HTMLclicks,
-    0 as unique_textclicks, 
     uniqMergeIf(member_state, (event = 'message_click') AND (channel = 'email') AND (NOT is_machine)) AS unique_human_clicks,
     uniqMergeIf(member_state, (event = 'message_click') AND (channel = 'email') AND (is_machine)) AS unique_bot_clicks,
     0 AS revenue,  
-    0 AS order_count,  
+    0 AS order_count,
+    0 as grow_views,
     0 AS grow_conversion_count
-FROM {{ source('clickhouse', 'msg_totals_bysenddate_v') }} v --anx_stg1_mercury.msg_totals_bysenddate v
+FROM {{ source('clickhouse', 'msg_totals_bysenddate_v') }} v
 LEFT JOIN {{ source('ds_internal', 'customer_metadata') }} cm ON v.account_id = cm.pls_org_id
---ds_internal.customer_metadata cm  ON v.account_id = cm.pls_org_id
-WHERE --domain = 'event.campaignactivity'
-    --and platform = 'msg:na'
-    --and v.account_id = '986'
- --and v.send_date > '2025-01-01' AND '2025-01-31'  -- Filter by a relevant date range
-  send_date >= '2024-01-24' and send_date < '2024-02-25'
+WHERE domain = 'event.campaignactivity'
+and platform = 'msg:na'
+and v.account_id = isnull('{{ account_id }}',v.account_id)  -- Filter by account_id if provided
+and send_date >= cast('{{ start_date }}' as date) and send_date < cast('{{ end_date }}' as date)
 GROUP BY
     cm.brand,
     cm.geo,
     cm.Country,
     cm.industry,
-    v.account_id,
-    cm.account_name,
     toDate(toStartOfMonth(send_date)),
     v.campaign_type,
     v.campaign_type,
     v.channel 
-    limit 100
 )
 
 SELECT * FROM raw_data
--- dbt_benchmark/models/profile/subscribers.sql
+++
@@ -1,3 +0,0 @@
-SELECT top 1000 a.pk_g4_subscribers_id, a.p_environment, b.p_ms_orig_src
-FROM xyz_dms_cust_986.dbo.e_g4_subscribers a 
-left join xyz_dms_cust_986.dbo.e_g4_subscribers_p_ms_orig_src b on a.pk_g4_subscribers_id = b.pk_g4_subscribers_id

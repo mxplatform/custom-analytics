0f85bb51e37f6b2e742524fb087d2ea9310102a2
santosh_nayak <34561766+santoshnayak-mi@users.noreply.github.com>
2025-11-03 09:38:17 -0500

    Added few more to gitignore

--- dbt_benchmark/models/benchmark_new/benchmark_new.sql
+++ dbt_benchmark/models/benchmark_new/benchmark_new.sql
@@ -11,14 +11,14 @@ SELECT
     v.campaign_type,
     'Regular' as sto_type,
     v.channel as channel_type,
-    sumIf(1, (event = 'message_send')) AS sends, 
-    sumIf(1, ((event = 'message_soft_bounce') OR (event = 'message_hard_bounce'))) AS bounces,
-    sumIf(1, (event = 'message_soft_bounce')) AS unique_soft_bounces,
-    sumIf(1, (event = 'message_hard_bounce')) AS unique_hard_bounces,
+    sumIf(count, (event = 'message_send')) AS sends, 
+    sumIf(count, ((event = 'message_soft_bounce') OR (event = 'message_hard_bounce'))) AS bounces,
+    sumIf(count, (event = 'message_soft_bounce')) AS unique_soft_bounces,
+    sumIf(count, (event = 'message_hard_bounce')) AS unique_hard_bounces,
     uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email')) AS unique_opens,
-    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email')) AS unique_real_opens,
-    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email')) AS unique_precached_opens,
-    sumIf(1, (event = 'message_click')) AS total_clicks,
+    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email') AND NOT is_machine) AS unique_real_opens,
+    uniqMergeIf(member_state, (event = 'message_open') AND (channel = 'email') AND NOT is_machine) AS unique_precached_opens,
+    sumIf(count, (event = 'message_click')) AS total_clicks,
     uniqMergeIf(member_state, (event = 'message_click') AND (channel = 'email')) AS unique_clicks,
     uniqMergeIf(member_state, (event = 'message_click') AND (channel = 'email') AND (NOT is_machine)) AS unique_human_clicks,
     uniqMergeIf(member_state, (event = 'message_click') AND (channel = 'email') AND (is_machine)) AS unique_bot_clicks,
@@ -33,7 +33,7 @@ WHERE --domain = 'event.campaignactivity'
  --and platform = 'msg:na'
     --and v.account_id = '986'
  --and v.send_date > '2025-01-01' AND '2025-01-31'  -- Filter by a relevant date range
-  send_date >= '2022-01-24' and send_date < '2025-05-25'
+  send_date >= '{{ var("start_date", "{{ metadata.start_date  }}") }}') AND (send_date < '{{ var("end_date", "{{ metadata.end_date  }}") }}') 
 GROUP BY
     cm.brand,
     cm.geo,
